24ea082758ddf15ffe7b11400b56d851fe29dd75
jscrodrigo <cruz.jsrodrigo@gmail.com>
2020-06-25 20:24:34 -0300

    created the script for closing and expanding the modal.

---
+++ desafio2/script.js
@@ -0,0 +1,33 @@
+const modalOvarlay = document.querySelector('.modal-overlay');
+const modal  = document.querySelector('.modal');
+const closeButton = document.querySelector('.close-modal');
+const maximizeButton = document.querySelector('.expand-modal');
+const cards = document.querySelectorAll('.card');
+
+const close = () =>{
+  modalOvarlay.classList.remove('active');
+  modalOvarlay.querySelector('iframe').src ="";
+  modal.classList.remove('maximize');
+}
+
+
+for(let card of cards){
+  let cardId = card.getAttribute('id');
+  card.querySelector('.card-title').addEventListener('click', function(){
+    modalOvarlay.classList.add('active');
+    modalOvarlay.querySelector('iframe').src = `https://rocketseat.com.br/${cardId}`;
+  });
+}
+
+closeButton.addEventListener('click', close);
+
+maximizeButton.addEventListener('click', function(){
+  if(modal.classList.contains('maximize')) {
+    modal.classList.remove('maximize');
+  } else {
+    modal.classList.add('maximize');
+  }
+});
+
+
+
