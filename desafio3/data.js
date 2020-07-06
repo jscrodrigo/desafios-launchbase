8d159a52a53b3b8931cef1c8c2ee7604b3d2d07f
jscrodrigo <cruz.jsrodrigo@gmail.com>
2020-07-06 20:14:25 -0300

    This file holds the data for the content.njk page

---
+++ desafio3/data.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    id: "starter",
+    courseImage: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/starter.svg",
+    title: "Starter",
+    modules: "5 módulos",
+    price: "Gratuito"
+  },
+  {
+    id: "launchbase",
+    courseImage: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/launchbase.svg", 
+    title: "LaunchBase",
+    modules: "16 módulos",
+    price: "R$ 960,00"
+  },
+  {
+    id: "gostack",
+    courseImage: "https://storage.googleapis.com/golden-wind/bootcamp-launchbase/assets/gostack.svg", 
+    title: "GoStack",
+    modules: "13 módulos",
+    price: "R$ 2.500,00"
+  }
+]
