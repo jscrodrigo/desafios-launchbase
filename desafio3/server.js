195fd21e9e6a4a6e9cd183d1d1039f65bbae5579
jscrodrigo <cruz.jsrodrigo@gmail.com>
2020-07-09 17:23:36 -0300

    A new route was added

--- desafio3/server.js
+++ desafio3/server.js
@@ -8,12 +8,15 @@ myServer.use(express.static('public'))
 myServer.set('view engine', 'njk')
 
 nunjucks.configure('views', {
-  express:myServer
+  express:myServer,
+  noCache: true,
+  autoescape: false
 });
 
 myServer.get('/content', function(req, res) {
   return res.render('content', {cards: data})
 });
+
 
 myServer.get('/', function(req, res){
   return res.render('content', {cards: data})
@@ -23,8 +26,19 @@ myServer.get('/about', function(req, res) {
   return res.render('about')
 })
 
+myServer.get('/content/:id', function(req, res) {
+  const id = req.params.id;
+  const course = data.find(function(course){
+    return course.id == id
+  })
+  if(!course){
+    return res.send('Course not found...')
+  } 
+  return res.render('description', {course: course})
+})
+
 myServer.use(function(req, res) {
-  res.status(404).render('not-found');
+ res.status(404).render('not-found');
 });
 
 myServer.listen(5000, function() {
