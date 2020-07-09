63f1bbad818691960bd8f0f68d64a8de3beed00a
jscrodrigo <cruz.jsrodrigo@gmail.com>
2020-07-09 16:23:45 -0300

    Page redirectionament was included insted of modal

--- desafio3/public/script.js
+++ desafio3/public/script.js
@@ -1,30 +1,31 @@
 const cards = document.querySelectorAll('.card');
-const modalOvarlay = document.querySelector('.modal-overlay');
-const modal  = document.querySelector('.modal');
-const closeButton = document.querySelector('.close-modal');
-const maximizeButton = document.querySelector('.expand-modal');
+// const modalOvarlay = document.querySelector('.modal-overlay');
+// const modal  = document.querySelector('.modal');
+// const closeButton = document.querySelector('.close-modal');
+// const maximizeButton = document.querySelector('.expand-modal');
 
-const close = () =>{
-  modalOvarlay.classList.remove('active');
-  modalOvarlay.querySelector('iframe').src ="";
-  modal.classList.remove('maximize');
-}
+// const close = () =>{
+//   modalOvarlay.classList.remove('active');
+//   modalOvarlay.querySelector('iframe').src ="";
+//   modal.classList.remove('maximize');
+// }
 
-closeButton.addEventListener('click', close);
+// closeButton.addEventListener('click', close);
 
-maximizeButton.addEventListener('click', function(){
-  if(modal.classList.contains('maximize')) {
-    modal.classList.remove('maximize');
-  } else {
-    modal.classList.add('maximize');
-  }
-});
+// maximizeButton.addEventListener('click', function(){
+//   if(modal.classList.contains('maximize')) {
+//     modal.classList.remove('maximize');
+//   } else {
+//     modal.classList.add('maximize');
+//   }
+// });
 
 for(let card of cards){
   let cardId = card.getAttribute('id');
   card.querySelector('.card-title').addEventListener('click', function(){
-    modalOvarlay.classList.add('active');
-    modalOvarlay.querySelector('iframe').src = `https://rocketseat.com.br/${cardId}`;
+    // modalOvarlay.classList.add('active');
+    // modalOvarlay.querySelector('iframe').src = `https://rocketseat.com.br/${cardId}`;
+    window.location.href = `/content/${cardId}`
   });
 }
 
